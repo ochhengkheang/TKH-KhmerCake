c9021c9c792646b5e87e4c0ad7631273d43425e8
ochhengkheang <ochhengkheang@gmail.com>
2023-05-19 23:04:49 +0700

    update viewer

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -1,5 +1,5 @@
 @extends('admin.layouts.master')
-@section('title','Khmer Dessert Ecommerce-Products')
+@section('title','Product Management')
 @section('container-body')
     Product index.blade.php is under construction
 @endsection
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,5 @@
-site under construction
+@extends("master")
+@section("title","Khmer Dessert Ecommerce-Products")
+@section("container-body")
+    <h1>container is under construction</h1>
+@endsection
